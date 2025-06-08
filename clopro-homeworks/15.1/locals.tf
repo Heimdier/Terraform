7d83bbb5c79772835540f8fc3a13d832c72b95d3
Heimdier <57480070+Heimdier@users.noreply.github.com>
2025-06-08 11:13:57 +0500

    Update locals.tf

--- clopro-homeworks/15.1/locals.tf
+++ clopro-homeworks/15.1/locals.tf
@@ -1,6 +1,9 @@
 locals {
-  ssh_key = "${var.ssh_user}:${file("/home/maha/.ssh/yan.pub")}"
   vm_pub_name = "vm1-${var.subnet_publ_name}"
   vm_priv_name = "vm2-${var.subnet_priv_name}"
   vm_nat_name = "nat-${var.subnet_publ_name}"
+  vm_meta = {
+    serial-port-enable = 1
+    ssh-keys           = "${var.ssh_user}:${file("~/.ssh/yan.pub")}"
+  }
 }
